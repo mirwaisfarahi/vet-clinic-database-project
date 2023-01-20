68e03622e39a5def95f959cc414cd9ae90245859
Mirwais Farahi <waisfarahi@gmail.com>
2023-01-20 20:24:30 +0500

    Edit queries.sql File

--- queries.sql
+++ queries.sql
@@ -3,7 +3,7 @@
 SELECT * FROM animals WHERE name like '%mon';
 SELECT name FROM animals WHERE date_of_birth >= '2016-01-01' AND date_of_birth < '2019-12-31';
 SELECT name FROM animals WHERE neutered = 't' AND escape_attempts < 3;
-SELECT date_of_birth FROM animals where name LIKE '%Agumon' OR name LIKE '%Pikachu';
+SELECT date_of_birth FROM animals WHERE name = 'Pikachu' OR name = 'Agumon';
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = 't';
 SELECT * FROM animals WHERE name NOT LIKE '%Gabumon';
